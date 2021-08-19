ce6d0252f3dd5fbdd0f3868516031fb26ff52eac
goplanit <goplanit19@gmail.com>
2021-08-19 21:30:14 +0900

    wecode 1주차 자바스크립트 공부한 것

---
+++ array.js
@@ -0,0 +1,12 @@
+const productList = {
+  img: img,
+  brand: "brand",
+  title: "LGx에어틱스 1인용 컴퓨터 게이밍 책상 GD001 / 보조테이블 8종",
+  price:79000,
+  withPrice: true,
+  sale: 0.33,
+  star: 4.6,
+  review: 2532,
+}
+
+console.log(productList);
