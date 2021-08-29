6d87d11ffcae680b858c15a52a5ffee831a5a1a6
goplanit19@gmail.com <goplanit19@gmail.com>
2021-08-29 17:53:11 +0900

    ADD: 메모 연습

--- event_practice/index.js
+++ event_practice/index.js
@@ -1,19 +1,11 @@
-const thisIsPw = document.getElementById("password").value;
-const thisIsRePw = document.getElementById("re-password").value;
-const thisIsAlert = document.getElementById("alert");
-const thisIsButton = document.querySelector(".login-btn");
+const thisIsPw = document.getElementById("password");
+const thisIsRePw = document.getElementById("re-password");
+const thisIsAlert = document.querySelector(".alert");
 
-thisIsButton.addEventListener("click", function (event) {
-  console.log(thisIsPw);
-  console.log(thisIsRePw);
-
-  if (thisIsPw !== thisIsRePw) {
-    thisIsAlert.value = "비밀번호가 일치하지 않습니다";
-    return;
-  }
-
-  if (thisIsPw === thisIsRePw) {
-    thisIsAlert.value = " ";
-    return;
+thisIsRePw.addEventListener("keyup", function (event) {
+  if (thisIsPw.value !== thisIsRePw.value) {
+    thisIsAlert.innerHTML = "비밀번호가 일치하지 않습니다";
+  } else {
+    thisIsAlert.innerHTML = "";
   }
 });
